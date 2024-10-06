808d9d6c57560ea89e26ee4375b9ebdd2f29eb1c
Khaled <hy3113194@gmail.com>
2024-10-06 12:19:38 +0300

    test

--- lib/core/networking/dio_factory.dart
+++ lib/core/networking/dio_factory.dart
@@ -22,7 +22,7 @@ class DioFactory {
         ..options.connectTimeout = timeout
         ..options.sendTimeout = timeout;
 
-      //aaaa
+      //aaaaa
       await setDefaultHeaders(dio);
       addInterceptors(dio);
 
