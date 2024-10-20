bbc55325ef9a0e8026666acbf40823d996fcf578
OmarMohamed <omarmohamed01284@gmail.com>
2024-10-20 19:40:19 +0300

    For younes

--- lib/features/program_details/presentation/views/widgets/program_details_tab_bar_view.dart
+++ lib/features/program_details/presentation/views/widgets/program_details_tab_bar_view.dart
@@ -38,19 +38,16 @@ class _ProgramDetailsTabBarViewState extends State<ProgramDetailsTabBarView> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: _currentHeight,
-      child: TabBarView(
-        controller: widget.tabController,
-        children: [
-          const SingleChildScrollView(child: OverviewTabBarContent()),
-          const SupplementTabBarContent(),
-          SizedBox(height: 370.h, child: const PhotoGalleryTabBarContent()),
-          const Center(
-            child: Text('Reviews'),
-          ),
-        ],
-      ),
+    return TabBarView(
+      controller: widget.tabController,
+      children: [
+        OverviewTabBarContent(),OverviewTabBarContent(),OverviewTabBarContent(),OverviewTabBarContent(),
+        // const SupplementTabBarContent(),
+        // SizedBox(height: 370.h, child: const PhotoGalleryTabBarContent()),
+        // const Center(
+        //   child: Text('Reviews'),
+        // ),
+      ],
     );
   }
 }
