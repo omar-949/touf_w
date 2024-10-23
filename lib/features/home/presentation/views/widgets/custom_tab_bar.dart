2c1b0ba77c8ee969a03c3148a5ea2ac86aa9401e
Khaled <hy3113194@gmail.com>
2024-10-23 21:32:23 +0300

    enable home tab bar scrolling

--- lib/features/home/presentation/views/widgets/custom_tab_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_tab_bar.dart
@@ -27,7 +27,6 @@ class _CustomTabBarState extends State<CustomTabBar> {
       unselectedLabelColor: Colors.white,
       dividerColor: Colors.transparent,
       tabAlignment: TabAlignment.center,
-      physics: const NeverScrollableScrollPhysics(),
       onTap: (index) {
         setState(() {
           activeIndex = index;
